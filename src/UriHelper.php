b2e5bc9041247e1addd133cf8d167aaa49f93cea
Christian Stocker <me@chregu.tv>
2017-12-15 21:09:22 +0100

    add composeUri, decomposeUri and refactor parsing a little
    Should now also support when a dynamic stack has multiple operations with the same URL

--- src/UriHelper.php
+++ src/UriHelper.php
@@ -4,6 +4,9 @@ namespace Rokka\Client;
 
 use GuzzleHttp\Psr7\Uri;
 use Psr\Http\Message\UriInterface;
+use Rokka\Client\Core\Stack;
+use Rokka\Client\Core\StackOperation;
+use Rokka\Client\Core\StackUrl;
 
 class UriHelper
 {
@@ -36,48 +39,200 @@ class UriHelper
      */
     public static function addOptionsToUri(UriInterface $uri, $options)
     {
-        $path = $uri->getPath();
-        if (preg_match('#^/(?<stack>[^/]+)/(?<rest>[0-9a-f]{6,40}.*)$#', $path, $matches)) {
-            // nothing to do here
-        } elseif (preg_match('#^/(?<stack>[^/]+)/(?<options>[^/]+)/(?<rest>[0-9a-f]{6,40}.*)$#', $path, $matches)) {
-            $urlOptions = self::decomposeOptions($matches['options']);
-            $inputOptions = self::decomposeOptions($options);
-            $combinedOptions = array_replace_recursive($urlOptions, $inputOptions);
-            $newOptions = [];
-            foreach ($combinedOptions as $key => $values) {
-                $newOption = "$key";
-                foreach ($values as $k => $v) {
-                    $newOption .= "-$k-$v";
-                }
-                $newOptions[] = $newOption;
-            }
-            $options = implode('--', $newOptions);
-        } else {
+        $matches = self::decomposeUri($uri);
+        if (empty($matches)) {
             //if nothing matches, it's not a proper rokka URL, just return the original uri
             return $uri;
         }
-
-        return $uri->withPath('/'.$matches['stack'].'/'.$options.'/'.$matches['rest']);
+        $matches['stack'] = self::addOptionsToStackUrlObject($matches['stack'],$options);
+        return self::composeUri($matches, $uri);
     }
 
     /**
-     * @param string $options
+     * Generate a rokka uri with the array format returned by decomposeUri().
+     *
+     * The array config looks like
+     * ['stack' => 'stackname',
+     *  'hash' => 'hash',
+     *  'filename' => 'filename-for-url'
+     *  'format' => 'image format' # eg. jpg
+     *  'stack' => StackUrl object with options and operations # same methods as a Stack objects
+     * ]
+     *
+     * @since 1.2.0
+     *
+     * @param array        $components
+     * @param UriInterface $uri        If this is provided, it will change the path for that object
+     *
+     * @return UriInterface
+     */
+    public static function composeUri(array $components, UriInterface $uri = null)
+    {
+        /** @var StackUrl $stack */
+        $stack = $components['stack'];
+        $stackName = $stack->getName();
+        $path = '/'.$stackName;
+        $stackConfig = $stack->getConfigAsArray();
+        if ($stackName === 'dynamic') {
+            $path .= '/'.self::getUriStringFromStackConfig($stackConfig);
+        } else {
+            $stackUrl = self::getUriStringFromStackConfig($stackConfig);
+            if (!empty($stackUrl)) {
+                $path .= '/' . $stackUrl;
+            }
+        }
+        if (isset($components['hash']) && !empty($components['hash'])) {
+            $path .= '/'.$components['hash'];
+
+            if (isset($components['filename']) && !empty($components['filename'])) {
+                $path .= '/'.$components['filename'];
+            }
+
+            $path .= '.'.$components['format'];
+        }
+
+        if (null !== $uri) {
+            return  $uri->withPath($path);
+        }
+
+        return new Uri($path);
+    }
+
+    /**
+     * Return components of a rokka URL.
+     *
+     * @since 1.2.0
+     *
+     * @param UriInterface $uri
      *
      * @return array
      */
-    private static function decomposeOptions($options)
+    public static function decomposeUri(UriInterface $uri)
     {
-        $components = [];
-        foreach (explode('--', $options) as $stringOperation) {
-            $stringOperationWithOptions = explode('-', $stringOperation);
-            $stringOperationName = $stringOperationWithOptions[0];
-            if ('' == $stringOperationName) {
-                continue;
+        $path = $uri->getPath();
+        if (preg_match('#^/(?<stack>[^/]+)/(?<hash>[0-9a-f]{6,40})(?<rest>.*)$#', $path, $matches)) {
+        } elseif (preg_match('#^/(?<stack>[^/]+)/(?<combinedOptions>.+)/(?<hash>[0-9a-f]{6,40})(?<rest>.*)$#', $path, $matches)) {
+        }
+        if (0 === count($matches)) {
+            return [];
+        }
+        if (preg_match('#^/{0,1}(?<filename>[a-z\-\0-\9]*)\.(?<format>.{3,4}$)#', $matches['rest'], $matches2)) {
+            unset($matches['rest']);
+            $matches = array_merge($matches, $matches2);
+            if (empty($matches['filename'])) {
+                $matches['filename'] = null;
             }
-            $components[$stringOperationName] = self::parseOptions(array_slice($stringOperationWithOptions, 1));
+        }
+        $stack = new StackUrl(null, $matches['stack']);
+        if (isset($matches['combinedOptions'])) {
+            $stack = self::addOptionsToStackUrlObject($stack, $matches['combinedOptions']);
+            unset($matches['combinedOptions']);
+        }
+        foreach ($matches as $key => $value) {
+            if (is_int($key)) {
+                unset($matches[$key]);
+            }
+        }
+        $matches['stack'] = $stack;
+        return $matches;
+    }
+
+    /**
+     * @param string      $url
+     * @param string      $size
+     * @param null|string $custom
+     *
+     * @return UriInterface
+     */
+    public static function getSrcSetUrlString($url, $size, $custom = null)
+    {
+        return self::getSrcSetUrl($size, new Uri($url), $custom);
+    }
+
+    /**
+     * @param string       $size
+     * @param UriInterface $url
+     * @param null|string  $custom
+     *
+     * @return UriInterface
+     */
+    public static function getSrcSetUrl($size, UriInterface $url, $custom = null)
+    {
+        $identifier = substr($size, -1, 1);
+        $size = substr($size, 0, -1);
+        if ('x' === $identifier) {
+            $uri = self::addOptionsToUri($url, 'options-dpr-'.$size);
+        } elseif ('w' === $identifier) {
+            $uri = self::addOptionsToUri($url, 'resize-width-'.$size);
+        } else {
+            return $url;
+        }
+        if (null !== $custom) {
+            if (preg_match('#^([0-9]+)x$#', $custom, $matches)) {
+                $uri = self::addOptionsToUri($uri, 'options-dpr-'.$matches[1].'--resize-width-'.(int) ceil($size / $matches[1]));
+            } else {
+                $stack = self::addOptionsToStackUrlObject(new StackUrl(), $custom);
+                // if dpr is given in custom option, but not width, calculate correct width
+                $resizeOperations = $stack->getStackOperationsByName('resize');
+                $widthIsNotSet = true;
+                foreach($resizeOperations as $resizeOperation) {
+                    if (isset($resizeOperation->options['width'])) {
+                        $widthIsNotSet = false;
+                    }
+                }
+                $options = $stack->getStackOptions();
+                if (isset($options['dpr']) && $widthIsNotSet) {
+                    $custom .= '--resize-width-'.(int) ceil($size / $options['dpr']);
+                }
+
+                $uri = self::addOptionsToUri($uri, $custom);
+            }
         }
 
-        return $components;
+        return $uri;
+    }
+
+    /**
+     * @param StackUrl $stack
+     * @param string $options
+     *
+     * @return StackUrl
+     */
+    private static function addOptionsToStackUrlObject(StackUrl $stack, $options)
+    {
+
+        $part = 0;
+        // if stack already has operations we assume we don't want to add more, it's just overriding parameters
+        if (count($stack->getStackOperations()) > 0) {
+            $part++;
+        }
+        foreach (explode('/', $options) as $option) {
+            $part++;
+            foreach (explode('--', $option) as $stringOperation) {
+                $stringOperationWithOptions = explode('-', $stringOperation);
+                $stringOperationName = $stringOperationWithOptions[0];
+                if ('' == $stringOperationName) {
+                    continue;
+                }
+                $parsedOptions = self::parseOptions(array_slice($stringOperationWithOptions, 1));
+                if ('options' === $stringOperationName) {
+                    $stack->setStackOptions(array_merge($stack->getStackOptions(), $parsedOptions));
+                } else {
+                    // only add as stack operation everything before the first /
+                    if ($part === 1) {
+                        $stackOperation = new StackOperation($stringOperationName, $parsedOptions);
+                        $stack->addStackOperation($stackOperation);
+                    } else {
+                        $stackOperations = $stack->getStackOperationsByName($stringOperationName);
+                        foreach($stackOperations as $stackOperation) {
+                            $stackOperation->options = array_merge($stackOperation->options, $parsedOptions);
+                        }
+                    }
+                }
+            }
+        }
+
+        return $stack;
     }
 
     /**
@@ -100,5 +255,52 @@ class UriHelper
         }
 
         return array_combine($optionKeys, $optionValues);
+    }
+
+    /**
+     * @param array $config
+     *
+     * @return string
+     */
+    private static function getUriStringFromStackConfig(array $config)
+    {
+        $newOptions = [];
+        if (isset($config['operations'])) {
+            foreach ($config['operations'] as $values) {
+                $newOptions[] = self::getStringForOptions($values['name'], $values['options']);
+            }
+        }
+
+        $newStackOptions = null;
+        if (isset($config['options'])) {
+            $newStackOptions = self::getStringForOptions('options', $config['options']);
+        }
+        //don't return this, if it's only "options" as string
+        if ($newStackOptions !== 'options') {
+            $newOptions[] = $newStackOptions;
+        }
+        $options = implode('--', $newOptions);
+
+        return $options;
+    }
+
+    /**
+     * @param string $name
+     * @param string $values
+     * @return string
+     */
+    private static function getStringForOptions($name, $values)
+    {
+        $newOption = $name;
+        ksort($values);
+        foreach ($values as $k => $v) {
+            if ($v === false) {
+                $v = 'false';
+            } else if ($v === true) {
+                $v = 'true';
+            }
+            $newOption .= "-$k-$v";
+        }
+        return $newOption;
     }
 }
--- tests/UriHelperTest.php
+++ tests/UriHelperTest.php
@@ -2,6 +2,10 @@
 
 namespace Rokka\Client\Tests;
 
+use GuzzleHttp\Psr7\Uri;
+use Rokka\Client\Core\Stack;
+use Rokka\Client\Core\StackOperation;
+use Rokka\Client\Core\StackUrl;
 use Rokka\Client\UriHelper;
 
 class UriHelperTest extends \PHPUnit_Framework_TestCase
@@ -13,10 +17,82 @@ class UriHelperTest extends \PHPUnit_Framework_TestCase
     {
         return [
             ['https://test.rokka.io/stackname/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg', 'options-dpr-2', 'https://test.rokka.io/stackname/options-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['https://test.rokka.io/stackname/b53763.jpg', 'options-dpr-2', 'https://test.rokka.io/stackname/options-dpr-2/b53763.jpg'],
             ['https://test.rokka.io/stackname/resize-width-100/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg', 'options-dpr-2', 'https://test.rokka.io/stackname/resize-width-100--options-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
-            ['https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg', 'options-dpr-2', 'https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['https://test.rokka.io/stackname/resize-width-100/options-dpr-3/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg', 'options-dpr-2', 'https://test.rokka.io/stackname/resize-width-100--options-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['https://test.rokka.io/stackname/resize-width-100/options-dpr-3/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg', '', 'https://test.rokka.io/stackname/resize-width-100--options-dpr-3/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['https://test.rokka.io/dynamic/resize-width-100/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg', 'options-dpr-2', 'https://test.rokka.io/dynamic/resize-width-100--options-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['https://test.rokka.io/dynamic/resize-width-100--resize-width-200/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg', 'options-dpr-2', 'https://test.rokka.io/dynamic/resize-width-100--resize-width-200--options-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
             ['https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true-dpr-3/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg', 'options-dpr-2', 'https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
             ['https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true-dpr-3/b537639e539efcc3df4459ef87c5963aa5079ca6/seo.jpg', 'options-dpr-2', 'https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6/seo.jpg'],
+            ['https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true-dpr-3/resize-height-200/b537639e539efcc3df4459ef87c5963aa5079ca6/seo.jpg', 'options-dpr-2', 'https://test.rokka.io/dynamic/resize-height-200-width-100--options-autoformat-true-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6/seo.jpg'],
+            ['https://test.rokka.io/dynamic/resize-width-100--resize-width-300--options-autoformat-true-dpr-3/resize-height-200/b537639e539efcc3df4459ef87c5963aa5079ca6/seo.jpg', 'options-dpr-2', 'https://test.rokka.io/dynamic/resize-height-200-width-100--resize-height-200-width-300--options-autoformat-true-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6/seo.jpg'],
+            ['https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true/b53763.jpg', 'options-dpr-2', 'https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true-dpr-2/b53763.jpg'],
+            ['https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true-dpr-3/b53763/seo.jpg', 'options-dpr-2', 'https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true-dpr-2/b53763/seo.jpg'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function provideGetSrcSetUrl()
+    {
+        return [
+            ['2x', null, 'https://test.rokka.io/stackname/options-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['3x', null, 'https://test.rokka.io/stackname/options-dpr-3/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['300w', null, 'https://test.rokka.io/stackname/resize-width-300/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['300w', '2x', 'https://test.rokka.io/stackname/resize-width-150--options-dpr-2/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['300w', '3x', 'https://test.rokka.io/stackname/resize-width-100--options-dpr-3/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['2x', 'options-jpg.quality-50', 'https://test.rokka.io/stackname/options-dpr-2-jpg.quality-50/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['300w', 'options-jpg.quality-50', 'https://test.rokka.io/stackname/resize-width-300--options-jpg.quality-50/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+
+            ['300w', 'options-jpg.quality-50-dpr-2', 'https://test.rokka.io/stackname/resize-width-150--options-dpr-2-jpg.quality-50/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['300w', 'options-jpg.quality-50-dpr-2--resize-width-200', 'https://test.rokka.io/stackname/resize-width-200--options-dpr-2-jpg.quality-50/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+            ['300w', 'options-jpg.quality-50--resize-width-200', 'https://test.rokka.io/stackname/resize-width-200--options-jpg.quality-50/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function provideDecomposeUri()
+    {
+        return [
+            ['https://test.rokka.io/stackname/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg',
+                ['hash' => 'b537639e539efcc3df4459ef87c5963aa5079ca6', 'filename' => null, 'format' => 'jpg',
+                    'stackurl' => 'stackname', 'stackoptions' => []]
+            ],
+            ['https://test.rokka.io/stackname/resize-width-100/b537639e5.jpg',
+                ['hash' => 'b537639e5', 'filename' => null, 'format' => 'jpg',
+                    'stackurl' => 'stackname/resize-width-100', 'stackoptions' => []]
+            ],
+            ['https://test.rokka.io/stackname/resize-width-100--rotate-angle-20/b537639e5.jpg',
+                ['hash' => 'b537639e5', 'filename' => null, 'format' => 'jpg',
+                    'stackurl' => 'stackname/resize-width-100--rotate-angle-20', 'stackoptions' => []]
+            ],
+            ['https://test.rokka.io/dynamic/resize-height-200-width-100--options-jpg.quality-80/b53763/seo-test.webp',
+                ['hash' => 'b53763', 'filename' => 'seo-test', 'format' => 'webp',
+                    'stackurl' => 'dynamic/resize-height-200-width-100--options-jpg.quality-80', 'stackoptions' => ['jpg.quality' => '80']]
+            ],
+            ['https://test.rokka.io/dynamic/options-autoformat-true/b53763/seo-test.webp',
+                ['hash' => 'b53763', 'filename' => 'seo-test', 'format' => 'webp',
+                    'stackurl' => 'dynamic/options-autoformat-true', 'stackoptions' => ['autoformat' => 'true']]
+            ],
+            ['https://test.rokka.io/dynamic/options-autoformat-true/options-dpr-2/b53763/seo-test.webp',
+                ['hash' => 'b53763', 'filename' => 'seo-test', 'format' => 'webp',
+                    'stackurl' => 'dynamic/options-autoformat-true-dpr-2',  'stackoptions' => ['autoformat' => 'true', 'dpr' => '2']],
+                'https://test.rokka.io/dynamic/options-autoformat-true-dpr-2/b53763/seo-test.webp'
+            ],
+            ['https://test.rokka.io/dynamic/resize-width-100--options-autoformat-true/resize-width-200/b53763/seo-test.webp',
+                ['hash' => 'b53763', 'filename' => 'seo-test', 'format' => 'webp',
+                    'stackurl' => 'dynamic/resize-width-200--options-autoformat-true', 'stackoptions' => ['autoformat' => 'true']],
+                'https://test.rokka.io/dynamic/resize-width-200--options-autoformat-true/b53763/seo-test.webp'
+            ],
+            ['https://test.rokka.io/dynamic/resize-width-100--resize-width-300--options-autoformat-true/resize-width-200/b53763/seo-test.webp',
+                ['hash' => 'b53763', 'filename' => 'seo-test', 'format' => 'webp',
+                    'stackurl' => 'dynamic/resize-width-200--resize-width-200--options-autoformat-true','stackoptions' => ['autoformat' => 'true']],
+                'https://test.rokka.io/dynamic/resize-width-200--resize-width-200--options-autoformat-true/b53763/seo-test.webp'
+            ],
         ];
     }
 
@@ -30,5 +106,52 @@ class UriHelperTest extends \PHPUnit_Framework_TestCase
     public function testAddOptionsToUri($inputUrl, $options, $expected)
     {
         $this->assertSame($expected, UriHelper::addOptionsToUriString($inputUrl, $options));
+    }
+
+    public function testGetDynamicStackFromStackObject()
+    {
+        $stack = new Stack(null, 'dynamic');
+
+        $stack->addStackOperation(new StackOperation('resize', ['width' => 200, 'height' => 200]));
+        $stack->addStackOperation(new StackOperation('rotate', ['angle' => 45]));
+        $stack->setStackOptions(['jpg.quality' => 80]);
+        $stack->addStackOption('webp.quality', 80);
+        $this->assertEquals('dynamic/resize-height-200-width-200--rotate-angle-45--options-jpg.quality-80-webp.quality-80', $stack->getStackUrl());
+    }
+
+    /**
+     * @dataProvider provideDecomposeUri
+     *
+     * @param string $inputUrl
+     * @param array  $expected
+     */
+    public function testDecomposeUri($inputUrl, $expected, $expectedComposeUrl = null)
+    {
+        $uri = new Uri($inputUrl);
+        $components = UriHelper::decomposeUri($uri);
+        $componentsTmp = $components;
+        /** @var StackUrl $stack */
+        $stack = $componentsTmp['stack'];
+        $componentsTmp['stackurl'] = $stack->getStackUrl();
+        unset($componentsTmp['stack']);
+        $componentsTmp['stackoptions'] = $stack->getStackOptions();
+        $this->assertSame($expected, $componentsTmp);
+        if ($expectedComposeUrl === null) {
+            $expectedComposeUrl = $inputUrl;
+        }
+        $this->assertSame((string) UriHelper::composeUri($components, $uri), $expectedComposeUrl);
+    }
+
+    /**
+     * @dataProvider provideGetSrcSetUrl
+     *
+     * @param $size
+     * @param $custom
+     * @param $expected
+     */
+    public function testGetSrcSetUrl($size, $custom, $expected)
+    {
+        $inputUrl = 'https://test.rokka.io/stackname/b537639e539efcc3df4459ef87c5963aa5079ca6.jpg';
+        $this->assertSame($expected, (string) UriHelper::getSrcSetUrlString($inputUrl, $size, $custom));
     }
 }
