212b5e309610d35ffa79f381961374c6aee94bc0
Christian Stocker <me@chregu.tv>
2017-12-16 14:13:07 +0100

    add tests

---
+++ tests/Core/UriComponentTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Rokka\Client\Tests;
+
+use Rokka\Client\Core\StackUri;
+use Rokka\Client\Core\UriComponents;
+
+class UriComponentTest extends \PHPUnit_Framework_TestCase
+{
+    public function provideCreateFromArray()
+    {
+        return [
+            'simple stack' => [['stack' => 'foo', 'filename' => 'test', 'hash' => '34d345', 'format' => 'jpg'], 'foo'],
+            'dynamic stack' => [['stack' => 'dynamic/options-dpr-2', 'filename' => 'test', 'hash' => '34d345', 'format' => 'jpg'], 'dynamic'],
+            'StackUri object' => [['stack' => new StackUri('foo'), 'filename' => 'test', 'hash' => '34d345', 'format' => 'jpg'], 'foo'],
+        ];
+    }
+
+    /**
+     * @dataProvider provideCreateFromArray
+     *
+     * @param array $config
+     * @param $name
+     */
+    public function testCreateFromArray($config, $name)
+    {
+        $components = UriComponents::createFromArray($config);
+        $stack = $components->getStack();
+        $this->assertEquals($name, $stack->getName());
+        $this->assertEquals($config['filename'], $components->getFilename());
+        $this->assertEquals($config['hash'], $components->getHash());
+        $this->assertEquals($config['format'], $components->getFormat());
+    }
+}
