527c360eced90a730f0fb232cc4477111fce66f0
Christian Stocker <me@chregu.tv>
2018-10-25 10:58:48 +0200

    try with latest sami.phar

--- scripts/run_sami_docs_on_travis.sh
+++ scripts/run_sami_docs_on_travis.sh
@@ -5,7 +5,7 @@ git stash
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git remote update
 
-curl -s -o /tmp/sami.phar http://get.sensiolabs.org/sami.phar
+curl -s -o /tmp/sami.phar https://s3.eu-central-1.amazonaws.com/rokka-support-files/sami.phar
 
 mkdir -p ./sami-output/build/client-php-api/
 cp scripts/redirect_sami.html ./sami-output/build/client-php-api/index.html
