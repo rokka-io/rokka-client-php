4024e47be355715694ce066394c16e318d112bfa
Christian Stocker <me@chregu.tv>
2018-10-25 10:24:24 +0200

    always download sami and run it on php 7.2

--- scripts/run_sami_docs_on_travis.sh
+++ scripts/run_sami_docs_on_travis.sh
@@ -5,9 +5,8 @@ git stash
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git remote update
 
-if [[ ! -f  /tmp/sami.phar ]]; then
-    curl -s -o /tmp/sami.phar http://get.sensiolabs.org/sami.phar
-fi
+curl -s -o /tmp/sami.phar http://get.sensiolabs.org/sami.phar
+
 mkdir -p ./sami-output/build/client-php-api/
 cp scripts/redirect_sami.html ./sami-output/build/client-php-api/index.html
 cp -r scripts/sami_highlight /tmp/
