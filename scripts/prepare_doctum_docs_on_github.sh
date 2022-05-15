30d64a5440c5358177d21c24f8d89ff14bad4ede
Christian Stocker <me@chregu.tv>
2022-05-15 23:28:31 +0200

    template fun with doctum

--- scripts/prepare_doctum_docs_on_github.sh
+++ scripts/prepare_doctum_docs_on_github.sh
@@ -5,5 +5,7 @@ git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git remote update
 mkdir -p ./doctum-output/build/client-php-api/
 cp scripts/redirect_doctum.html ./doctum-output/build/client-php-api/index.html
+mkdir ./template/
+cp -r scripts/doctum_highlight ./template/
 
 exit 0
