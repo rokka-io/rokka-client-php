c2cee53cae9ae41eed495e0bde30abd26ed8cd5d
Christian Stocker <me@chregu.tv>
2022-05-15 23:16:56 +0200

    read this

--- scripts/prepare_doctum_docs_on_github.sh
+++ scripts/prepare_doctum_docs_on_github.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-
+git stash
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+git remote update
 mkdir -p ./doctum-output/build/client-php-api/
 cp scripts/redirect_doctum.html ./doctum-output/build/client-php-api/index.html
 
