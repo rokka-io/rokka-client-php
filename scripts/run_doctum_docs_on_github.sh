14150470db535cf7b836b7e694767979a51a58ab
Christian Stocker <me@chregu.tv>
2022-05-15 23:00:33 +0200

    try with sudo

--- scripts/run_doctum_docs_on_github.sh
+++ scripts/run_doctum_docs_on_github.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
 
+
 git stash
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git remote update
 
-cd /tmp/
 
 if [ ! -f /tmp/doctum.phar ]; then
     # Download the latest 5.x release
     curl -s -o /tmp/doctum.phar https://doctum.long-term.support/releases/5/doctum.phar
     rm -f /tmp/doctum.phar.sha256
     curl -s -o /tmp/doctum.phar.sha256 https://doctum.long-term.support/releases/5/doctum.phar.sha256
+    cd /tmp/
 
     sha256sum --strict --check /tmp/doctum.phar.sha256
     rm -f /tmp/doctum.phar.sha256
+    cd $OLDPWD
     # You can fetch the latest version code here:
     # https://doctum.long-term.support/releases/5/VERSION
 fi
